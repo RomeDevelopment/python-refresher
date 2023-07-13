bc7518276dd96a8a78ba8eceec3abdb9d297a78d
RomeDevelopment <rome.henry.bowman@gmail.com>
2023-07-13 10:10:41 -0400

    added edge cases for functions

--- physics.py
+++ physics.py
@@ -1,6 +1,9 @@
 # calculates buoyancy in Newtons given Volume in cubic meters and density in kilograms per meters cubed
 def calculate_buoyancy(V, density_fluid):
-    return density_fluid * 9.81 * V
+    if V or density_fluid <=0:
+        raise(ValueError)
+    else:
+        return density_fluid * 9.81 * V
 
 
 # let's you know in simple terms if something will float, returns none if equal
@@ -11,8 +14,14 @@ def will_it_float(V, mass):
         return False
     elif 1000 * V == mass:
         return None
-
+    elif V or mass <= 0:
+        raise(ValueError)
 
 # tells you the pressure in pascals at any depth
 def calculate_pressure(depth):
-    return depth * 9.81 * 1000
+    if depth<0:
+        raise(ValueError)
+    elif depth=0:
+        return(101325)
+    else:
+        return depth * 9.81 * 1000
