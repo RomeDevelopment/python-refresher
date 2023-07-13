e88e4a67d7d2401be99677be54a860ce63c9a647
RomeDevelopment <rome.henry.bowman@gmail.com>
2023-07-13 10:25:56 -0400

    got bank to save to git

--- bank.py
+++ bank.py
@@ -1,18 +1,17 @@
 class bank:
-    def __init__(self, balance, name ,account_number):
-        self.balance= balance
-        self.name= name
-        self.account_number=account_number
-        
+    def __init__(self, balance, name, account_number):
+        self.balance = balance
+        self.name = name
+        self.account_number = account_number
 
-        def withdraw(self,w):
-            if w <= balance:
-                self.balance=self.balance -w
-            else:
-                return("ERROR")
+    def withdraw(self, w):
+        if w <= balance:
+            self.balance = self.balance - w
+        else:
+            return "ERROR"
 
-        def deposit(self, d):
-            self.balance=self.balance+d
-        
-        def printbal(self):
-            print(self.balance)
+    def deposit(self, d):
+        self.balance = self.balance + d
+
+    def printbal(self):
+        print(self.balance)
