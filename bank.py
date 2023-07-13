62fa7fb093c3a1b56973bae8341fece199054ef5
RomeDevelopment <rome.henry.bowman@gmail.com>
2023-07-13 09:04:22 -0400

    made bank class

---
+++ bank.py
@@ -0,0 +1,18 @@
+class bank:
+    def __init__(self, balance, name ,account_number):
+        self.balance= balance
+        self.name= name
+        self.account_number=account_number
+        
+
+        def withdraw(self,w):
+            if w <= balance:
+                self.balance=self.balance -w
+            else:
+                return("ERROR")
+
+        def deposit(self, d):
+            self.balance=self.balance+d
+        
+        def printbal(self):
+            print(self.balance)
